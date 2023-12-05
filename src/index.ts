195bdadad995372b839cf26b9bd8ee4c1186f8ab
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2023-12-05 16:49:58 +0700

    feat : express fileupload setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import fileUpload from 'express-fileupload';
 
 import userRoute from './routes/user.router';
 dotenv.config();
@@ -9,6 +10,9 @@ const app: express.Application = express();
 const PORT: string = process.env.PORT ?? '5000';
 
 app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.static('./public'));
 app.use(userRoute);
 
 app.listen(PORT, () => {
